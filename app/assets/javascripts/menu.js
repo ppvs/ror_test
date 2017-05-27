631f7c9fa37fe2967dff8a4fa5003f949e49840b
vityap <ppvs@mail.ru>
2017-05-28 00:08:26 +0300

    1

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,3 +1,13 @@
 /**
  * Created by vityap on 28.5.17.
  */
+function openMenu(id) {
+    overlay = document.getElementById("overlay");
+    if (document.getElementById(id).style.display == "none"){
+        document.getElementById(id).style.display = "block";
+        overlay.style.display = "block";
+    } else {
+        document.getElementById(id).style.display = "none";
+        overlay.style.display = "none";
+    }
+}
